3dbee73de71c672ac8f2d603073f81432ba042f8
Loris <loris4526@gmail.com>
2017-02-18 00:06:38 +0100

    Wire up EmployeeCreate for new employees. Rende Render names inside EmployeeList

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from "redux";
 
 import EmployeeFormReducer from "./EmployeeFormReducer";
 import AuthReducer from "./AuthReducer";
+import EmployeeReducer from "./EmployeeReducer";
 
 export default combineReducers({
   employeeForm: EmployeeFormReducer,
-  auth: AuthReducer
+  auth: AuthReducer,
+  employees: EmployeeReducer
 })
